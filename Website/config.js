1509cbde29c9a2ea0dbb4d068667b7f5ea067688
OmkarRetail <omkarretailventure@gmail.com>
2026-05-27 13:04:37 +0530

    Added new files

---
+++ Website/config.js
@@ -0,0 +1,28 @@
+window.OMKAR_SITE_CONFIG = {
+  companyName: "OMKAR RETAIL VENTURES",
+  domain: "omkarretailventure.in",
+  industry: "Zepto Dark Store Hiring",
+  location: "Bangalore, Karnataka, India",
+  phone: "9986362446",
+  whatsappNumber: "919986362446",
+  email: "omkarretailventure@gmail.com",
+  officeAddress:
+    "OMKAR RETAIL VENTURES - Zepto Dark Store Hiring Desk, Bangalore, Karnataka, India",
+  social: {
+    instagram: "https://instagram.com/",
+    facebook: "https://facebook.com/",
+    linkedin: "https://linkedin.com/"
+  },
+  analyticsMeasurementId: "",
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  },
+  web3formsAccessKey: "",
+  adminPin: "1234"
+};
+
